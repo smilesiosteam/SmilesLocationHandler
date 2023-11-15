d4582209b124c649da9521d50125a9664759e1d6
SmilesDev <smilesiosteam@gmail.com>
2023-11-15 18:27:23 +0500

    feat: added closure to handle detect location pop actions

--- Sources/SmilesLocationHandler/ViewControllers/DetectLocationPopup/SmilesDetectLocationPopUp.swift
+++ Sources/SmilesLocationHandler/ViewControllers/DetectLocationPopup/SmilesDetectLocationPopUp.swift
@@ -1,6 +1,6 @@
 //
 //  SmilesExplorerPickTicketPopUp.swift
-//  
+//
 //
 //  Created by Ghullam  Abbas on 15/11/2023.
 //
@@ -10,61 +10,75 @@ import SmilesLanguageManager
 import SmilesFontsManager
 
 
- final class SmilesDetectLocationPopUp: UIViewController {
-
+final class SmilesDetectLocationPopUp: UIViewController {
+    
     // MARK: - IBOutlets
     @IBOutlet private weak var mainContainerView: UIView!
     @IBOutlet private weak var messageLabel: UILabel!
     @IBOutlet private weak var imageIcon: UIImageView!
     @IBOutlet private weak var detectButton: UIButton!
     @IBOutlet private weak var searchButton: UIButton!
-
+    
+    // MARK: - Properties
+    private var detectLocationAction: (() -> Void)?
+    private var searchLocationAction: (() -> Void)?
+    
     // MARK: - View Controller Lifecycle
     public override func viewDidLoad() {
         super.viewDidLoad()
         styleFontAndTextColor()
         setupUI()
     }
-
+    
     // MARK: - Methods
     init() {
         super.init(nibName: "SmilesDetectLocationPopUp", bundle: .module)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     func configure(with viewModel: DetectLocationPopupViewModel?) {
         self.messageLabel.text = viewModel?.data?.message
         self.imageIcon.image = UIImage(named: viewModel?.data?.iconImage ?? "")
         self.detectButton.setTitle(viewModel?.data?.detectButtonTitle, for: .normal)
         self.searchButton.setTitle(viewModel?.data?.searchButtonTitle, for: .normal)
     }
-
+    
     private func styleFontAndTextColor() {
         self.messageLabel.fontTextStyle = .smilesHeadline4
         self.detectButton.fontTextStyle = .smilesHeadline4
         self.searchButton.fontTextStyle = .smilesBody4
     }
-
+    
     private func setupUI() {
         mainContainerView.clipsToBounds = true
         mainContainerView.layer.cornerRadius = 16
         mainContainerView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
         view.backgroundColor = .appRevampFilterTextColor.withAlphaComponent(0.6)
     }
-
+    
     // MARK: - IBActions
     @IBAction private func didTabDetectLocationButton(_ sender: UIButton) {
+        detectLocationAction?()
         self.dismiss(animated: true)
     }
-
+    
     @IBAction private func didTabSearchLocationButton(_ sender: UIButton) {
+        searchLocationAction?()
         self.dismiss(animated: true)
     }
-
+    
     @IBAction private func didTabCrossButton(_ sender: UIButton) {
         self.dismiss(animated: true)
+    }
+    // MARK: - methods to set closure actions
+    func setDetectLocationAction(_ action: (() -> Void)?) {
+        detectLocationAction = action
+    }
+    
+    func setSearchLocationAction(_ action: (() -> Void)?) {
+        searchLocationAction = action
     }
 }
