b46a3cb7f07a44de18562feb47f0cee26db89cdb
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-11-17 13:01:28 +0500

    - Added missing models

---
+++ Sources/SmilesLocationHandler/Models/SWGoogleAddress/SWGoogleBound.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 17/11/2023.
+//
+
+import Foundation
---
+++ Sources/SmilesLocationHandler/Views/CustomViews/LocationPinView/LocationPinView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationPinView.swift
+//  
+//
+//  Created by Abdul Rehman Amjad on 17/11/2023.
+//
+
+import UIKit
+
+class LocationPinView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
